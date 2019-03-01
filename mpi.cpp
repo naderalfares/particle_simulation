9c7bf96073b68b74f6db0ef021184226377586b0
ashirbadm <ashirbadm@gmail.com>
2019-03-01 18:53:38 -0500

    module for packing data for communication

--- mpi.cpp
+++ mpi.cpp
@@ -10,28 +10,30 @@
 struct proc_info {
     int xLow, xHigh;
     int yLow, yHigh;
+    int gxHigh, gxLow; // CELL_SIZE if at edge else 0
+    int gyHigh, gyLow; // same
 };
 
 
-
+// particles: the particles in the boundary of the processor and not the ghost particles
 void packing(particle_t** particles, int n, struct proc_info * procs_info, int n_proc,
                 int** partition_offset, int** partition_sizes){
     
-    particle_t* new_particles = (particle_t *) malloc(len * sizeof(particle_t));
+    particle_t* new_particles = (particle_t *) malloc(n * sizeof(particle_t));
     int index = 0, count = 0;
     for(int i = 0; i < n_proc; i ++){
         *partition_offset[i] = index;
         count = 0;
-        int xHi = proc_info[i].xHigh;
-        int xLo = proc_info[i].xLow;
-        int yHi = proc_info[i].yHigh;
-        int yLo = proc_info[i].yLow;
+        int xHi = procs_info[i].xHigh + procs_info[i].gxHigh;
+        int xLo = procs_info[i].xLow + procs_info[i].gxLow;
+        int yHi = procs_info[i].yHigh + procs_info[i].gyHigh;
+        int yLo = procs_info[i].yLow + procs_info[i].gyLow;
         for(int j = 0; j < n; j++){
-            int xCord = particles[j].x;
-            int yCord = particles[j].y;
+            int xCord = particles[j]->x;
+            int yCord = particles[j]->y;
             // compare particle coordiantes to processor boundaries
-            if(xCord < xHi and xCord > xLo and ycord < yHi and yCord > yLo ) {
-                new_particles[index++] = particles[j];
+            if(xCord < xHi and xCord > xLo and yCord < yHi and yCord > yLo ) {
+                new_particles[index++] = *particles[j];
                 count++;
             }
             
@@ -44,7 +46,9 @@ void packing(particle_t** particles, int n, struct proc_info * procs_info, int n
    
 };
 
+void communicateData(particle_t *particles, int *partitionSizes, int *partitionOffsets, int *) {
 
+}
 
 
 //
